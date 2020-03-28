370c2d93e4a7a5ab3c2e2df9ce6624bac33e1c08
ssubedi2046 <ssubedi2046@gmail.com>
2020-03-28 16:49:17 -0500

    Add files via upload

---
+++ cicdpipeline/src/cicdpipeline/Start.java
@@ -0,0 +1,14 @@
+package cicdpipeline;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee e=new Employee();
+		e.setName("Sushil");
+		e.setAddress("Prospect Heights");
+		System.out.println(e.getName()+"-"+e.getAddress());
+	}
+
+}
